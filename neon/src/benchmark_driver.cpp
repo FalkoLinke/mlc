43b9419bfbf1160b39af57f4b7b640f029b46fca
Johann Schwarze <johann.schwarze@q-17.de>
2026-04-17 10:40:11 +0200

    returning number of executed instructions from fmadd_kernel

--- neon/src/benchmark_driver.cpp
+++ neon/src/benchmark_driver.cpp
@@ -1,10 +1,58 @@
 #include <iostream>
+#include <chrono>
+
 #include "benchmark_kernel.h"
+
+typedef int(kernel_func_t)();
+
+struct kernel_t {
+public:
+    kernel_func_t* const func;
+    std::string const name;
+
+    kernel_t(kernel_func_t* const func, std::string const name) : func(func), name(name) {
+
+    }
+
+    int call() const {
+        return func();
+    }
+};
+
+#define MAKE_KERNEL(FUNC) ( kernel_t( (FUNC), #FUNC ) )
+
+
+
+
+
+
+
+
+
+
+void handle_kernel(kernel_t const kernel, int const repetitions) {
+    std::cout << "========================" << std::endl;
+    std::cout << "Benchmarking \"" << kernel.name "\"" << std::endl;
+
+    
+    
+}
+
+
+
 
 
 
 int main() {
-    int insts_count = fmadd_kernel();
-    std::cout << "Instructions count: " << insts_count << std::endl;
+    kernel_t kernels[] = {
+        MAKE_KERNEL(fmadd_kernel),
+    };
+    size_t kernels_count = sizeof(kernels) / sizeof(kernel_t);
+    int repetitions = 20;
+
+    for (int i = 0; i < kernels_count; i++) {
+        handle_kernel(kernels[i], repetitions);
+    }
+
     return 0;
 }
--- neon/src/benchmark_kernel.s
+++ neon/src/benchmark_kernel.s
@@ -6,30 +6,27 @@
 ;
     .global _fmadd_kernel
 _fmadd_kernel:
-    mov s0, #0
+    movi v0.4s, #0
 
 insts_start:
-    .rept   50
+    .rept   200
+    fmadd s0, s0, s0, s0
     fmadd s0, s0, s0, s0
     .endr
 insts_end:
 
-    mov x1, insts_count
+    adr x1, insts_count
     ldr x0, [x1]
     ret
-
-
-
-
-
-
-
-
-
-
-    .section "rodata"
 insts_count:
-    .long insts_end - insts_start
+    .long (insts_end - insts_start) / 4
+
+
+
+
+
+
+
 
 
 
