dd396958a7b7d1b4a29bbd949ce4641802ac6523
Falko Linke <falkolinke@gmail.com>
2026-05-04 12:01:14 +0200

    make gemm work for 32x32

--- sme/src/gemm_32_32_1_kernel.s
+++ sme/src/gemm_32_32_1_kernel.s
@@ -5,7 +5,7 @@
 #endif /* __APPLE__ */
 
 
-    .text
+.text
 
 
 
@@ -28,6 +28,92 @@
 */
     .global FUNCLABEL(gemm_32_32_1)
 FUNCLABEL(gemm_32_32_1):
+
+    // C stride
+    mov x30, x5
+    lsr x30, x30, #1
+    // first za reg
+    mov w12, #0
+    mov x6, x3
+
+    smstart
+    ptrue p0.s
+
+    // load C
+load_C_loop:
+    subs x30, x30, #1
+
+    ldr za[w12], [x6, #0, mul vl]
+    ldr za[w12, #1], [x6, #1, mul vl]
+
+    ldr za[w12, #2], [x6, #31, mul vl]
+    ldr za[w12, #3], [x6, #32, mul vl]
+
+    add w12, w12, #4
+    addvl, x6, x6, #4
+
+    cbnz x30, load_C_loop
+
+
+    // load A z0 and B z2 16 floats at a time and perform the outer product
+    ld1w {z0.s}, p0/z, [x0, #0, mul vl]
+    ld1w {z2.s}, p0/z, [x1, #0, mul vl]
+    fmopa za0.s, p0/m, p0/m, z0.s, z2.s
+    
+
+    // load A z1 and B z3 16 floats at a time and perform the outer product
+    ld1w {z1.s}, p0/z, [x0, #1, mul vl]
+    ld1w {z3.s}, p0/z, [x1, #1, mul vl]
+    fmopa za3.s, p0/m, p0/m, z1.s, z3.s
+
+    fmopa za1.s, p0/m, p0/m, z1.s, z2.s
+    
+    fmopa za2.s, p0/m, p0/m, z0.s, z3.s
+
+
+
+    // store the results back to C
+    mov x30, x5
+    mov w12, #0
+
+store_C_loop:
+    subs x30, x30, #1
+
+    ldr za[w12], [x6, #0, mul vl]
+    ldr za[w12, #1], [x6, #1, mul vl]
+
+    ldr za[w12, #2], [x6, #31, mul vl]
+    ldr za[w12, #3], [x6, #32, mul vl]
+
+    add w12, w12, #4
+    addvl, x6, x6, #4
+
+    cbnz x30, store
+
+    smstop
+    
+    ret
+
+
+/*
+       /**
+    * @brief GEMM that computes: C+=AB.
+    * @param a    Pointer to column-major matrix A.
+    * @param b    Pointer to row-major matrix B.
+    * @param c    Pointer to column-major matrix C.
+    * @param ld_a Leading dimension of A.
+    * @param ld_b Leading dimension of B.
+    * @param ld_c Leading dimension of C.
+    **/
+   void gemm_512_16_16( float   const * a,
+                      float   const * b,
+                      float         * c,
+                      int64_t         ld_a,
+                      int64_t         ld_b,
+                      int64_t         ld_c );
+*/
+    .global FUNCLABEL(gemm_512_16_16)
+FUNCLABEL(gemm_512_16_16):
 
     // C stride
     mov x30, x5
--- sme/src/gemm_driver.cpp
+++ sme/src/gemm_driver.cpp
@@ -2,7 +2,7 @@
 #include "identity_kernel.h"
 
 extern "C" {
-       /**
+    /**
     * @brief GEMM that computes: C+=AB.
     * @param a    Pointer to column-major matrix A.
     * @param b    Pointer to row-major matrix B.
@@ -17,6 +17,20 @@ extern "C" {
                       int64_t         ld_a,
                       int64_t         ld_b,
                       int64_t         ld_c );
+
+ /**
+ * @brief Identity operation with m=16 and n=16.
+ * @param a       Pointer to column-major matrix A.
+ * @param b       Pointer to matrix B.
+ * @param ld_a    Leading dimension of A.
+ * @param ld_b    Leading dimension of B.
+ * @param trans_b Column-major B if 0, row-major B if 1. 
+ **/
+void identity_16_16( float const * a,
+                    float       * b,
+                    int64_t       ld_a,
+                    int64_t       ld_b,
+                    int32_t       trans_b );
 
 }
 
