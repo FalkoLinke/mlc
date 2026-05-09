697aa49843fabc1d0111680e56d728f7721b1935
Johann Schwarze <johann.schwarze@q-17.de>
2026-05-09 03:09:19 +0200

    testing sme implementation of zero_kernel

--- sme/src/zero_kernel.s
+++ sme/src/zero_kernel.s
@@ -17,14 +17,15 @@ void zero_16_16( float* a,
     .global FUNCLABEL(zero_16_16)
 FUNCLABEL(zero_16_16):
     smstart
-    fmov z0.s, #0.0
+    zero { za0.b }
+    mov w12, #0
     ptrue p0.s
     
     mov x2, #16
 zero_16_16_loop01:
     cbz x2, zero_16_16_end01
     
-    st1w z0.s, p0, [x0]
+    st1w za0h.s[w12, 0], p0, [x0]
 
     add x0, x0, x1, LSL #2
     subs x2, x2, #1
