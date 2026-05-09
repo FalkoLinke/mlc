80bf7593c6354d56317a9d007573244e4dcf8c0b
Johann Schwarze <johann.schwarze@q-17.de>
2026-05-09 03:25:56 +0200

    implemented identity using SME instructions

--- sme/src/identity_kernel.s
+++ sme/src/identity_kernel.s
@@ -83,74 +83,57 @@ void identity_16_16( float const * a,
 FUNCLABEL(identity_16_16):
     stp x29, x30, [sp, #-16]!
     mov fp, sp
-
-    cbz x4, handle_notrans
-    b handle_trans
-handle_notrans:
-    bl FUNCLABEL(identity_16_16_notrans)
-    ldp x29, x30, [sp], #16
-    ret
-handle_trans:
     smstart
 
-    add x13, x2, x2
-    add x14, x13, x2
-    add x7, x3, x3
-    add x15, x7, x3
+    cbz x4, identity_16_16_notrans
+    b identity_16_16_trans
+identity_16_16_notrans:
 
-    ptrue p0.s, VL4
+    ptrue p0.s
+    mov x5, #16
+identity_16_16_loop01:
+    cbz x5, identity_16_16_loop01_end
 
-    // perform copy
-    mov x9, x0              // x9 and x10 point to A
-    mov x11, x1             // x11 and x12 point to B
-    mov x5, #4              // x5 and x6 are loop counters
-loop01:
-    cbz x5, end01
-
-    mov x10, x9
-    mov x12, x11
-
-    .rept 4
-    // perform submatrix transpose, writing to the correct target submatrix
-    mov x0, x10
-    mov x1, x12
-
-// identity_4_4_start
-    // load A
     ld1w z0.s, p0/z, [x0]
-    ld1w z1.s, p0/z, [x0, x2, LSL #2]
-    ld1w z2.s, p0/z, [x0, x13, LSL #2]
-    ld1w z3.s, p0/z, [x0, x14, LSL #2]
+    st1w z0.s, p0, [x1]
 
-    // perform transpose on 2x2 submatrices
-    trn1 z4.s, z0.s, z1.s
-    trn2 z5.s, z0.s, z1.s
-    trn1 z6.s, z2.s, z3.s
-    trn2 z7.s, z2.s, z3.s
-
-    // transpose matrix of submatrices
-    trn1 z16.d, z4.d, z6.d
-    trn1 z17.d, z5.d, z7.d
-    trn2 z18.d, z4.d, z6.d
-    trn2 z19.d, z5.d, z7.d
-
-    // store result
-    st1w z16.s, p0, [x1]
-    st1w z17.s, p0, [x1, x3, LSL #2]
-    st1w z18.s, p0, [x1, x7, LSL #2]
-    st1w z19.s, p0, [x1, x15, LSL #2]
-// identity_4_4_end
-
-    add x12, x12, x3, LSL #4    // B pointer changes based on transpose flag
-    add x10, x10, #16           // A pointer changes normally
-    .endr
-
-    add x11, x11, #16       // B pointer changes based on transpose flag
-    add x9, x9, x2, LSL #4  // A pointer changes normally
+    add x0, x0, x2, LSL #2
+    add x1, x1, x3, LSL #2
     subs x5, x5, #1
-    b loop01
-end01:
+    b identity_16_16_loop01
+identity_16_16_loop01_end:
 
+    b identity_16_16_ret
+identity_16_16_trans:
+
+    ptrue p0.s
+    mov w12, #0
+    mov x5, #16
+identity_16_16_loop02:
+    cbz x5, identity_16_16_loop02_end
+
+    ld1w za0h.s[w12, 0], p0/z, [x0]
+
+    add w12, w12, #1
+    add x0, x0, x2, LSL #2
+    subs x5, x5, #1
+    b identity_16_16_loop02
+identity_16_16_loop02_end:
+
+    mov w12, #0
+    mov x5, #16
+identity_16_16_loop03:
+    cbz x5, identity_16_16_loop03_end
+
+    st1w za0v.s[w12, 0], p0, [x1]
+
+    add w12, w12, #1
+    add x1, x1, x3, LSL #2
+    subs x5, x5, #1
+    b identity_16_16_loop03
+identity_16_16_loop03_end:
+
+identity_16_16_ret:
     smstop
     ldp x29, x30, [sp], #16
     ret
@@ -159,77 +142,6 @@ end01:
 
 
 
-
-
-
-
-
-
-
-FUNCLABEL(identity_16_16_notrans):
-    stp x29, x30, [sp, #-16]!
-    mov fp, sp
-
-    .rept 16
-    ldr x4, [x0, #0]
-    ldr x5, [x0, #8]
-    ldr x6, [x0, #16]
-    ldr x7, [x0, #24]
-    ldr x9, [x0, #32]
-    ldr x10, [x0, #40]
-    ldr x11, [x0, #48]
-    ldr x12, [x0, #56]
-    add x0, x0, x2, LSL #2
-    str x4, [x1, #0]
-    str x5, [x1, #8]
-    str x6, [x1, #16]
-    str x7, [x1, #24]
-    str x9, [x1, #32]
-    str x10, [x1, #40]
-    str x11, [x1, #48]
-    str x12, [x1, #56]
-    add x1, x1, x3, LSL #2
-    .endr
-
-    ldp x29, x30, [sp], #16
-    ret
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-Notes:
-
-A = [
-    [0, 1, 2, 3],
-    [4, 5, 6, 7],
-    [8, 9, A, B],
-    [C, D, E, F]
-]
-A^T = [
-    [0, 4, 8, C],
-    [1, 5, 9, D],
-    [2, 6, A, E],
-    [3, 7, B, F]
-]
-
-
-v0 = [0, 4, 2, 6]
-v1 = [1, 5, 3, 7]
-v2 = [8, C, A, E]
-v3 = [9, D, B, F]
-
-
-
-*/
 
 
 
