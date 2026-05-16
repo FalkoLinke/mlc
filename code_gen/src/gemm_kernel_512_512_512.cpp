26bbf6e1f43f764a9e0fa4342655b649e1a9ff2c
Falko Linke <falkolinke@gmail.com>
2026-05-16 13:13:03 +0200

    refactor: improve ldr_za and str_za functions, update generate_gemm_kernel_512_512_512 logic

--- code_gen/src/gemm_kernel_512_512_512.cpp
+++ code_gen/src/gemm_kernel_512_512_512.cpp
@@ -12,66 +12,79 @@ using shift_kind_t = InstGen::shift_kind_t;
 using sme_hv_kind_t = InstGen::sme_hv_kind_t;
 
 // LDR ZA[Wv, #imm], [Xn, #imm, MUL VL]
-uint32_t ldr_za(int wv, int imm, int xn) {
-    uint32_t rv = wv - 12; // Wv muss W12-W15 sein
-    uint32_t inst = 0xE1008000;
-    inst |= (rv << 13);
-    inst |= ((xn & 0x1f) << 5);
-    inst |= (imm & 0xf);
+uint32_t ldr_za(int wv, int off4, int xn) {
+    uint32_t inst = 0xE1000000;
 
-    return inst;
-};
-
-// STR ZA[Wv, #imm], [Xn, #imm, MUL VL]
-uint32_t str_za(int wv, int imm, int xn) {
     uint32_t rv = wv - 12;
-    uint32_t inst = 0xE1208000;
-    inst |= (rv << 13);
+
+    // Rv auf Bits 14:13
+    inst |= ((rv & 0x3) << 13);
+    
+    // Xn (Rn) auf Bits 9:5
     inst |= ((xn & 0x1f) << 5);
-    inst |= (imm & 0xf);
+    
+    // Immediate-Wert auf Bits 3:0
+    inst |= (off4 & 0xf);
+    
+    return inst;
+}
+
+uint32_t str_za(int wv, int off4, int xn) {
+    uint32_t inst = 0xE1200000; 
+    
+    // Rv auf Bits 14:13 (wv - 12, da wv in [12,15] liegt)
+    uint32_t rv = (wv - 12) & 0x3;
+    inst |= (rv << 13);              
+
+    //Xn Basis-Register (Bits 9:5)
+    inst |= ((xn & 0x1F) << 5);
+
+    //Immediate-Offset (off4) auf Bits 3:0 (off4 muss in [0,15] liegen)
+    inst |= (off4 & 0xF);
 
     return inst;
-};
+}
 
 // LDR Zt, [Xn, #imm, MUL VL]
 uint32_t ldr_z(int zt, int xn, int imm) {
-    uint32_t inst = 0x85804000;
-    inst |= (zt & 0x1f);
-    inst |= ((xn & 0x1f) << 5);
-    inst |= ((imm & 7) << 10);
-    inst |= ((imm & 0x1f8) << 13);
 
+    uint32_t inst = 0x85804000;
+
+    inst |= (zt & 0x1F);                     
+    inst |= ((xn & 0x1F) << 5);             
+    inst |= ((imm & 0x3) << 10);            
+    inst |= ((imm & 0x1F8) << 16);   
     return inst;
-};
+}
 
 // FMOPA ZAda.S, Pn/M, Pm/M, Zn.S, Zm.S (FP32 non-widening)
 uint32_t fmopa(int zada, int pn, int pm, int zn, int zm) {
-    uint32_t inst = 0x80A00000;
+
+    uint32_t inst = 0x80800000;
     inst |= ((zm & 0x1f) << 16);
     inst |= ((pm & 0x7) << 13);
     inst |= ((pn & 0x7) << 10);
     inst |= ((zn & 0x1f) << 5);
     inst |= (zada & 0x3);
-    
+
     return inst;
 };
-
 
 /**
  * Generates a GEMM kernel for 512x512x512 matrices.
  */
-void const * generate_gemm_kernel_512_512_512() {
+void generate_gemm_kernel_512_512_512(mini_jit::Kernel& kernel) {
 
     mini_jit::InstGen gen;
-    mini_jit::Kernel kernel;
 
     auto reg_x = [](uint32_t id) { return static_cast<gpr_t>(id | 0x20); };
     auto reg_w = [](uint32_t id) { return static_cast<gpr_t>(id); };
 
 
+    //kernel.add_instr(gen.base_smstart());
+    kernel.add_instr(0xd503477f);
+    kernel.add_instr(gen.ssve_ptrue(pr_t::p0, sve_size_t::s)); // ptrue p0.s
 
-    kernel.add_instr(gen.base_smstart());
-    kernel.add_instr(gen.ssve_ptrue(pr_t::p0, sve_size_t::s, InstGen::pr_pattern_t::pow2, 0)); // pow2 ?
     kernel.add_instr(0x04BF5829); // rdsvl x9, #1
     kernel.add_instr(0xD342FD29); // lsr x9, x9, #2  (UBFM)
     
@@ -100,21 +113,21 @@ void const * generate_gemm_kernel_512_512_512() {
 
      // Load C (Teil 1) - .rept 8 
     for (int i = 0; i < 8; ++i) {
-        ldr_za(12, 0, 6);
-        ldr_za(12, 1, 6);
+        kernel.add_instr(ldr_za(12, 0, 6));
+        kernel.add_instr(ldr_za(12, 1, 6));
         kernel.add_instr(gen.base_add(reg_w(12), reg_w(12), 4, 0));
         kernel.add_instr(gen.base_add(reg_x(6), reg_x(6), reg_x(10), shift_kind_t::lsl, 0, 0));
 
-        ldr_za(12, 0, 6);
-        ldr_za(12, 1, 6);
+        kernel.add_instr(ldr_za(12, 0, 6));
+        kernel.add_instr(ldr_za(12, 1, 6));
         kernel.add_instr(gen.base_add(reg_w(12), reg_w(12), 4, 0));
         kernel.add_instr(gen.base_add(reg_x(6), reg_x(6), reg_x(10), shift_kind_t::lsl, 0, 0));
     }
 
     // Load C (Teil 2) - .rept 8
     for (int i = 0; i < 8; ++i) {
-        ldr_za(13, 0, 6);
-        ldr_za(13, 1, 6);
+        kernel.add_instr(ldr_za(13, 0, 6));
+        kernel.add_instr(ldr_za(13, 1, 6));
         kernel.add_instr(gen.base_add(reg_w(13), reg_w(13), 4, 0));
         kernel.add_instr(gen.base_add(reg_x(6), reg_x(6), reg_x(10), shift_kind_t::lsl, 0, 0));
 
@@ -140,18 +153,18 @@ void const * generate_gemm_kernel_512_512_512() {
     // tile 0
     kernel.add_instr(ldr_z(0, 7, 0)); // ldr z0, [x7, #0, mul vl]
     kernel.add_instr(ldr_z(2, 8, 0)); // ldr z2, [x8, #0, mul vl]
-    fmopa(0, 0, 0, 2, 0); // fmopa za0.s, p0/m, p0/m, z2.s, z0.s
+    kernel.add_instr(fmopa(0, 0, 0, 2, 0)); // fmopa za0.s, p0/m, p0/m, z2.s, z0.s
 
     // tile 1
     kernel.add_instr(ldr_z(1, 7, 1)); // ldr z1, [x7, #1, mul vl]
-    fmopa(1, 0, 0, 2, 1); // fmopa za1.s, p0/m, p0/m, z2.s, z1.s
+    kernel.add_instr(fmopa(1, 0, 0, 2, 1)); // fmopa za1.s, p0/m, p0/m, z2.s, z1.s
 
     // tile 2
     kernel.add_instr(ldr_z(3, 8, 1)); // ldr z3, [x8, #1, mul vl]
-    fmopa(2, 0, 0, 3, 0); // fmopa za2.s, p0/m, p0/m, z3.s, z0.s
+    kernel.add_instr(fmopa(2, 0, 0, 3, 0)); // fmopa za2.s, p0/m, p0/m, z3.s, z0.s
     
     // tile 3
-    fmopa(3, 0, 0, 3, 1); // fmopa za3.s, p0/m, p0/m, z3.s, z1.s
+    kernel.add_instr(fmopa(3, 0, 0, 3, 1)); // fmopa za3.s, p0/m, p0/m, z3.s, z1.s
 
     // move to the next K tile
     // A is column-major, so we move in K dimension by adding ld_a (x3) to the pointer
@@ -159,7 +172,7 @@ void const * generate_gemm_kernel_512_512_512() {
     kernel.add_instr(gen.base_add(reg_x(8), reg_x(8), reg_x(17), shift_kind_t::lsl, 0, 0)); // add x8, x8, x17
 
     kernel.add_instr(gen.base_sub(reg_x(14), reg_x(14), 1, 1)); // subs x14, x14, #1
-    kernel.add_branch(gen.base_b_cond("N_loop", InstGen::br_cond_t(1))); // AArch64 Bedingung: NE = 1; // b.ne N_loop
+    kernel.add_branch(gen.base_b_cond("K_loop", InstGen::br_cond_t(1))); // AArch64 Bedingung: NE = 1; // b.ne N_loop
 
     // store the results back to C
     kernel.add_instr(gen.base_mov(reg_x(6), reg_x(2))); // mov x6, x2
@@ -211,10 +224,11 @@ void const * generate_gemm_kernel_512_512_512() {
 
 
     kernel.add_instr(gen.base_smstop());
+    kernel.add_instr(0xd503467f);
+    
+    // kernel.add_instr(0xd65f03c0);
+    kernel.add_instr(gen.base_ret());
 
-    kernel.add_instr(gen.base_ret() );
-
-//  l_kernel.write( "gemm_512_512_512.bin" );
+    //kernel.write( "gemm_512_512_512.bin" );
     kernel.set_kernel();
-    return kernel.get_kernel();
 }
