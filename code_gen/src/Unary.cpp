1419825fdd84f8b160cc86670f08e00737a67f26
Johann Schwarze <johann.schwarze@q-17.de>
2026-05-21 00:35:05 +0200

    extracted JIT benchmarks into separate executable

--- code_gen/src/Unary.cpp
+++ code_gen/src/Unary.cpp
@@ -40,9 +40,7 @@ Unary::error_t Unary::generate_identity_notrans_fp32(uint32_t m, uint32_t n) {
     kernel.add_instr(ig.ssve_ptrue(pr_t::p0, sve_size_t::s));
 
     uint32_t lrow = 0;
-    uint32_t lcol = 0;
     uint32_t srow = 0;
-    uint32_t scol = 0;
     uint32_t remaining_values = m * n;
     while (remaining_values > 0) {
         // fetch incoming values and store them in the registers on the JIT-stack
@@ -55,7 +53,6 @@ Unary::error_t Unary::generate_identity_notrans_fp32(uint32_t m, uint32_t n) {
 
             lrow += 16;
             if (lrow >= m) {
-                lcol += 1;
                 lrow = 0;
                 kernel.add_instr(ig.base_add(gpr_t::x0, gpr_t::x0, gpr_t::x2, shift_kind_t::lsl, 2));
             }
@@ -69,7 +66,6 @@ Unary::error_t Unary::generate_identity_notrans_fp32(uint32_t m, uint32_t n) {
 
             srow += 16;
             if (srow >= m) {
-                scol += 1;
                 srow = 0;
                 kernel.add_instr(ig.base_add(gpr_t::x1, gpr_t::x1, gpr_t::x3, shift_kind_t::lsl, 2));
             }
--- code_gen/src/unary_benchmarks_driver.cpp
+++ code_gen/src/unary_benchmarks_driver.cpp
@@ -2,10 +2,7 @@
 #include <chrono>
 
 #include "unary_kernels.h"
-#include "Unary.h"
 
-
-using mini_jit::Unary;
 
 int counter = 0;
 
@@ -134,50 +131,6 @@ void benchmark_kernel(kernel_t kernel, long const reps) {
 
 
 
-void benchmark_jit_kernel(uint64_t m, uint64_t n, Unary::ptype_t op, bool trans_b, long const reps) {
-    uint64_t ma = m;
-    uint64_t na = n;
-    uint64_t mb = trans_b ? n : m;
-    uint64_t nb = trans_b ? m : n;
-
-    int64_t lda = ma;
-    int64_t ldb = mb;
-
-    std::vector<float> a(na * lda, 0.0f);
-    std::vector<float> b(nb * ldb, 0.0f);
-
-    Unary unary;
-    unary.generate(m, n, trans_b, Unary::dtype_t::fp32, op);
-    Unary::kernel_t kernel_func = unary.get_kernel();
-
-    kernel_func(a.data(), b.data(), lda, ldb);
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    for (long i = 0; i < reps; i++) {
-        kernel_func(a.data(), b.data(), lda, ldb);
-        counter += 1;
-    }
-    
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
-
-
-    double gib_transferred = (reps / 1073741824.0f) * (m * n * sizeof(float));
-    double time_taken = duration.count();
-    double gibs = gib_transferred / time_taken;
-
-
-    std::cout << "=========================" << std::endl;
-    std::cout << "\tOp: " << (uint32_t)op << std::endl;
-    std::cout << "\t(m, n): (" << m << ", " << n << ")" << std::endl;
-    std::cout << "\ttrans_b: " << trans_b << std::endl;
-    std::cout << "\tDuration [s]: " << time_taken << std::endl;
-    std::cout << "\tBytes transferred [GiB]: " << gib_transferred << std::endl;
-    std::cout << "\tGiBs: " << gibs << std::endl;
-}
-
-
 
 
 
@@ -205,30 +158,11 @@ void benchmark_kernels(long const reps) {
     }
 }
 
-void benchmark_jit_kernels(long const reps) {
-    std::vector<uint64_t> ms = {64, 128, 512};
-    std::vector<uint64_t> ns = {64, 128, 512};
-    std::vector<bool> trans_bs = {false, true};
-    std::vector<Unary::ptype_t> ops = {Unary::ptype_t::identity, Unary::ptype_t::zero, Unary::ptype_t::relu};
-
-    for (Unary::ptype_t op : ops) {
-        for (bool trans_b : trans_bs) {
-            for (uint64_t m : ms) {
-                for (uint64_t n : ns) {
-                    benchmark_jit_kernel(m, n, op, trans_b, reps);
-                }
-            }
-        }
-    }
-}
-
 int main() {
     generate_kernels();
-    //long const reps = 500000000;
-    long const reps = 1000000;
+    long const reps = 100000000;
 
     benchmark_kernels(reps);
-    benchmark_jit_kernels(reps);
 
     std::cerr << counter << std::endl;
     return 0;
