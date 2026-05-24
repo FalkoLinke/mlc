0997b27d6e7e90bcc222a70386f0a0899798c846
Johann Schwarze <johann.schwarze@q-17.de>
2026-05-24 15:37:14 +0200

    added scalar and tiled lowerings for zero in TEIR compiler

--- teir/src/teir_compiler.cpp
+++ teir/src/teir_compiler.cpp
@@ -17,28 +17,6 @@ using mini_jit::Gemm;
 
 
 
-void teir_compiler::prepare_primitive(teir_operation const& operation, teir_primitive const& primitive) {
-    if (primitive.ptype == teir_ptype_t::ptype_zero) {
-        if (primitive.axes.at("M").size() == 1 && primitive.axes.at("N").size() == 1) {
-            teir_axis const* axis_m = operation.resolve_axis_id(primitive.axes.at("M")[0]);
-            teir_axis const* axis_n = operation.resolve_axis_id(primitive.axes.at("N")[0]);
-
-            std::unique_ptr<Unary> unary = std::make_unique<Unary>();
-            Unary::error_t err = unary->generate(axis_m->extent, axis_n->extent, false, Unary::dtype_t::fp32, Unary::ptype_t::zero);
-            if (err != Unary::error_t::success) {
-                std::cout << "Failed to generate kernel" << std::endl;
-            }
-            Unary::kernel_t kernel = unary->get_kernel();
-
-            unary_kernels.emplace_back(std::move(unary));
-            kernel_functions.push_back((void*)kernel);
-        }
-    }
-}
-
-
-
-
 
 void teir_compiler::iterate(teir_operation const& operation, std::string const& node, std::vector<teir_axis const*> axis_path, std::vector<InstGen::gpr_t> index_path) {
     // TODO: handle bit length constraints on offsets, strides and extents
@@ -119,42 +97,87 @@ void teir_compiler::iterate(teir_operation const& operation, std::string const& 
 void teir_compiler::invoke(teir_operation const& operation, teir_inv_node const* inv_node, std::vector<teir_axis const*> axis_path, std::vector<mini_jit::InstGen::gpr_t> index_path) {
     teir_primitive const* primitive = operation.resolve_primitive_id(inv_node->primitive);
 
-    // resolve primitive arguments
+    bool success = false;
+    
+    success = lower_zero_scalar(operation, *primitive);
+    if (success) {
+        return;
+    }
+
+    success = lower_zero_tile(operation, *primitive);
+    if (success) {
+        return;
+    }
+
+    std::cout << "missing lowering" << std::endl;
+}
+
+
+
+
+
+std::vector<uint64_t> teir_compiler::resolve_tensor_labels(teir_operation const& operation, teir_primitive const& primitive) const { 
     std::vector<uint64_t> primitive_tensor_idxs;
-    for (std::string const& tensor_id : primitive->tensors) {
+    for (std::string const& tensor_id : primitive.tensors) {
         uint64_t tensor_idx = operation.resolve_tensor_id_idx(tensor_id);
         primitive_tensor_idxs.push_back(tensor_idx);
     }
+    return primitive_tensor_idxs;
+}
 
-    // prepare kernel arguments
-    if (primitive->ptype == teir_ptype_t::ptype_copy || primitive->ptype == teir_ptype_t::ptype_relu) {
-        // unary kernel signature
-        kernel.add_instr(ig.base_ldr(InstGen::gpr_t::x0, InstGen::gpr_t::x28, primitive_tensor_idxs[0] * 8, InstGen::addr_mode_t::unsigned_offset));
-        kernel.add_instr(ig.base_ldr(InstGen::gpr_t::x1, InstGen::gpr_t::x28, primitive_tensor_idxs[1] * 8, InstGen::addr_mode_t::unsigned_offset));
-        kernel.add_instr(ig.base_movz(InstGen::gpr_t::x2, 0));
-        kernel.add_instr(ig.base_movz(InstGen::gpr_t::x3, 0));
-        
-    } else if (primitive->ptype == teir_ptype_t::ptype_zero) {
-        // unary kernel signature
-        kernel.add_instr(ig.base_ldr(InstGen::gpr_t::x1, InstGen::gpr_t::x28, primitive_tensor_idxs[0] * 8, InstGen::addr_mode_t::unsigned_offset));
-        kernel.add_instr(ig.base_movz(InstGen::gpr_t::x3, 0));
-
-    } else if (primitive->ptype == teir_ptype_t::ptype_contract) {
-        // gemm kernel signature
-        kernel.add_instr(ig.base_ldr(InstGen::gpr_t::x0, InstGen::gpr_t::x28, primitive_tensor_idxs[0] * 8, InstGen::addr_mode_t::unsigned_offset));
-        kernel.add_instr(ig.base_ldr(InstGen::gpr_t::x1, InstGen::gpr_t::x28, primitive_tensor_idxs[1] * 8, InstGen::addr_mode_t::unsigned_offset));
-        kernel.add_instr(ig.base_ldr(InstGen::gpr_t::x2, InstGen::gpr_t::x28, primitive_tensor_idxs[2] * 8, InstGen::addr_mode_t::unsigned_offset));
-        kernel.add_instr(ig.base_movz(InstGen::gpr_t::x3, 0));
-        kernel.add_instr(ig.base_movz(InstGen::gpr_t::x4, 0));
-        kernel.add_instr(ig.base_movz(InstGen::gpr_t::x5, 0));
-
+bool teir_compiler::lower_zero_scalar(teir_operation const& operation, teir_primitive const& primitive) {
+    if (primitive.ptype != teir_ptype_t::ptype_zero) {
+        return false;
+    }
+    if (primitive.axes.at("M").size() != 0) {
+        return false;
+    }
+    if (primitive.axes.at("N").size() != 0) {
+        return false;
     }
 
-    // call the kernel
-    uint64_t primitive_idx = operation.resolve_primitive_id_idx(primitive->id);
+    std::vector<uint64_t> tensor_idxs = resolve_tensor_labels(operation, primitive);
+
+    kernel.add_instr(ig.base_movz(InstGen::gpr_t::w1, 0));
+    kernel.add_instr(ig.base_ldr(InstGen::gpr_t::x0, InstGen::gpr_t::x28, tensor_idxs[0] * 8, InstGen::addr_mode_t::unsigned_offset));
+    kernel.add_instr(ig.base_str(InstGen::gpr_t::w1, InstGen::gpr_t::x0, 0, InstGen::addr_mode_t::unsigned_offset));
+
+    return true;
+}
+
+bool teir_compiler::lower_zero_tile(teir_operation const& operation, teir_primitive const& primitive) {
+    if (primitive.ptype != teir_ptype_t::ptype_zero) {
+        return false;
+    }   
+    if (primitive.axes.at("M").size() != 1) {
+        return false;
+    }
+    if (primitive.axes.at("N").size() != 1) {
+        return false;
+    }
+    teir_axis const* axis_m = operation.resolve_axis_id(primitive.axes.at("M")[0]);
+    teir_axis const* axis_n = operation.resolve_axis_id(primitive.axes.at("N")[0]);
+    uint64_t primitive_idx = operation.resolve_primitive_id_idx(primitive.id);
+
+    std::vector<uint64_t> tensor_idxs = resolve_tensor_labels(operation, primitive);
+
+    if (axis_m->strides[tensor_idxs[0]] != 4) {
+        return false;
+    }
+
+    Unary::kernel_t kernel_function = unary_cache.get_kernel(axis_m->extent, axis_n->extent, false, Unary::dtype_t::fp32, Unary::ptype_t::zero);
+    kernel_functions[primitive_idx] = kernel_function;
+
+    kernel.add_instr(ig.base_movz(InstGen::gpr_t::x0, 0));
+    kernel.add_instr(ig.base_ldr(InstGen::gpr_t::x1, InstGen::gpr_t::x28, tensor_idxs[0] * 8, InstGen::addr_mode_t::unsigned_offset));
+    kernel.add_instr(ig.base_movz(InstGen::gpr_t::x2, 0));
+    kernel.add_instr(ig.base_movz(InstGen::gpr_t::x3, axis_n->strides[tensor_idxs[0]] / 4));
+
     kernel.add_instr(ig.base_movz(InstGen::gpr_t::x7, primitive_idx * 8));
     kernel.add_instr(ig.base_add(InstGen::gpr_t::x7, InstGen::gpr_t::x27, InstGen::gpr_t::x7));
     kernel.add_instr(ig.base_blr(InstGen::gpr_t::x7));
+
+    return true;
 }
 
 
@@ -173,14 +196,21 @@ void teir_compiler::compile(teir_operation const& operation) {
     // pointer to tensors in x28
     kernel.add_instr(ig.base_mov(InstGen::gpr_t::x28, InstGen::gpr_t::x0));
 
+    /*
     // compile kernels for the used primitives
     for (teir_primitive const& primitive : operation.primitives) {
         prepare_primitive(operation, primitive);
     }
+    */
+
+    // initialize the kernel dispatch table
+    for (teir_primitive const& primitive : operation.primitives) {
+        kernel_functions.push_back(nullptr);
+    }
 
     // pointer to kernel dispatch table in x27
     // 
-    // we make sure to store this after we finished processing the primitives to avoid the data pointer changing
+    // we make sure to store this after we finished initializing the table to avoid the data pointer changing
     // due to vector reallocations
     void** dispatch_table = kernel_functions.data();
     kernel.add_instr(ig.base_movk(InstGen::gpr_t::x27, ((uint64_t)dispatch_table) & 0xff));
