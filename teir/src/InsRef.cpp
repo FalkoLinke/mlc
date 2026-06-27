221721e594264c9627a8aff2418ee85ca3a08dbf
Johann Schwarze <johann.schwarze@q-17.de>
2026-06-27 14:46:58 +0200

    using code_gen library to build teir subdirectory

--- teir/src/InsRef.cpp
+++
@@ -1,5 +0,0 @@
-#include "InsRef.h"
-
-mini_jit::InsRef::InsRef(uint32_t idx, LabeledInstruction ins) : idx(idx), labeled_ins(ins) {
-    
-}
--- teir/src/LabeledInstruction.cpp
+++
@@ -1,6 +0,0 @@
-#include "LabeledInstruction.h"
-
-
-mini_jit::LabeledInstruction::LabeledInstruction(uint32_t const ins, std::string const label, int32_t bias, uint32_t offs_bits, uint32_t offs_shift) : ins(ins), label(label), bias(bias), offs_bits(offs_bits), offs_shift(offs_shift) {
-
-}
